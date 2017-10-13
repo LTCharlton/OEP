b730c878b432f5e3d078946125a909925e232744
LTCharlton <linser22@gmail.com>
2017-10-12 18:45:59 -0700

    first

---
+++ main/main/main.cpp
@@ -0,0 +1,19 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+#include "Headers.h"
+
+
+int main()
+{
+	int num;
+	cout << "hello";
+	cin >> num;
+	return 0;
+}
+
